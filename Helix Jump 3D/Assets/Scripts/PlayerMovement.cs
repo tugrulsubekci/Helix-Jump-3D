1f11e777c72cb8fd3960ae48d45d629cf3e589e6
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2022-06-17 11:59:46 +0300

    tower rotator and ball bounce are added

--- Helix Jump 3D/Assets/Scripts/PlayerMovement.cs
+++ Helix Jump 3D/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody playerRigidbody;
+    public int jumpForce;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
---
+++ Helix Jump 3D/Assets/Scripts/Rotator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private float inputX;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            inputX = Input.GetAxis("Mouse X");
+            transform.Rotate(Vector3.up * inputX * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
