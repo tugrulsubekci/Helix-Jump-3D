0a1895779590495a5aa045bdd8c0528def3414a4
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2022-06-21 21:07:58 +0300

    Platform destroying, particle system and one other destroying sound effect

--- Helix Jump 3D/Assets/Scripts/PlayerMovement.cs
+++ Helix Jump 3D/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public int jumpForce;
     private bool isCollisionEntered = false;
     public float gravityMultiplier;
+    
+    // particle system
+    public ParticleSystem speedParticle;
+    private float prePosition;
+    private float distance;
 
     private GameManager GameManager;
 
@@ -30,9 +35,13 @@ public class PlayerMovement : MonoBehaviour
         {
             gameProgressSlider.value = -transform.position.y;
         }
-        
-    }
+        distance = Mathf.Abs(prePosition - transform.position.y);
 
+        if (distance > 5.0f && !speedParticle.isPlaying)
+        {
+            speedParticle.Play();
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(!isCollisionEntered && !GameManager.gameOver && !GameManager.levelCompleted)
@@ -42,7 +51,19 @@ public class PlayerMovement : MonoBehaviour
             Invoke(nameof(IsCollisionEntered),0.2f);
             string materialName = collision.gameObject.GetComponent<MeshRenderer>().material.name;
             FindObjectOfType<AudioManager>().Play("Bounce");
-
+            prePosition = transform.position.y;
+            speedParticle.Stop();
+            speedParticle.Clear();
+            if (distance > 15.0f)
+            {
+                for (int i = 0; i < collision.gameObject.GetComponent<Transform>().parent.childCount - 1; i++)
+                {
+                    GameObject x = collision.gameObject.GetComponent<Transform>().parent.GetChild(i).gameObject;
+                    Destroy(x , 0.2f);
+                    FindObjectOfType<AudioManager>().Play("Destroy");
+                }
+                
+            }
             if (materialName == "Safe (Instance)")
             {
 
@@ -58,9 +79,7 @@ public class PlayerMovement : MonoBehaviour
                 FindObjectOfType<AudioManager>().Play("LevelCompleted");
             }
         }
-
     }
-
     private void IsCollisionEntered()
     {
         isCollisionEntered = false;
